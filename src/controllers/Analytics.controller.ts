3b8867ee5ec6f99c46dcf103a36be2eb1c7926e1
Dan Kersten <danielytics@users.noreply.github.com>
2025-06-09 21:26:24 +0100

    Initial work to make the SDK more secure and trusted

--- src/controllers/Analytics.controller.ts
+++ src/controllers/Analytics.controller.ts
@@ -1,72 +1,71 @@
-import { App } from '../app'
+import { App } from "../app";
 import { TonConnectObserver } from "../observers/TonConnect.observer";
 import { DocumentObserver } from "../observers/Document.observer";
-import {BACKEND_URL, STAGING_BACKEND_URL} from "../constants";
+import { BACKEND_URL, Events, STAGING_BACKEND_URL } from "../constants";
 import { TappsObserver } from "../observers/tapps/Tapps.observer";
-import {WebAppObserver} from "../observers/WebApp.observer";
-import {WebViewObserver} from "../observers/WebView.observer";
+import { WebAppObserver } from "../observers/WebApp.observer";
+import { WebViewObserver } from "../observers/WebView.observer";
 
 export class AnalyticsController {
-    private appModule: App;
-    private tonConnectObserver: TonConnectObserver;
-    private documentObserver: DocumentObserver;
-    private webAppObserver: WebAppObserver;
-    private webViewObserver: WebViewObserver;
-    private tappsObserver: TappsObserver;
+  private appModule: App;
+  private tonConnectObserver: TonConnectObserver;
+  private documentObserver: DocumentObserver;
+  private webAppObserver: WebAppObserver;
+  private webViewObserver: WebViewObserver;
+  private tappsObserver: TappsObserver;
 
-    private eventsThreshold: Record<string, number>
+  private eventsThreshold: Record<string, number>;
 
-    constructor(app: App) {
-        this.appModule = app;
+  constructor(app: App) {
+    this.appModule = app;
 
-        this.documentObserver = new DocumentObserver(this);
-        this.tonConnectObserver = new TonConnectObserver(this);
-        this.webAppObserver = new WebAppObserver(this);
-        this.webViewObserver = new WebViewObserver(this);
-        this.tappsObserver = new TappsObserver(this);
+    this.documentObserver = new DocumentObserver(this);
+    this.tonConnectObserver = new TonConnectObserver(this);
+    this.webAppObserver = new WebAppObserver(this);
+    this.webViewObserver = new WebViewObserver(this);
+    this.tappsObserver = new TappsObserver(this);
+  }
+
+  public async init() {
+    this.documentObserver.init();
+    this.tonConnectObserver.init();
+    this.webAppObserver.init();
+    this.webViewObserver.init();
+    this.tappsObserver.init();
+
+    try {
+      this.eventsThreshold = await (
+        await fetch(
+          (this.appModule.env === "STG" ? STAGING_BACKEND_URL : BACKEND_URL) + "events/threshold",
+          {
+            signal: AbortSignal.timeout(2000),
+          },
+        )
+      ).json();
+    } catch (e) {
+      this.eventsThreshold = {
+        "app-hide": 3,
+      };
+    }
+  }
+
+  public recordEvent(event_name: string, data?: Record<string, any>) {
+    this.appModule.recordEvent(event_name, data).catch((e) => console.error(e));
+  }
+
+  public collectEvent(event_name: Events, data?: Record<string, any>) {
+    if (this.eventsThreshold[event_name] === 0) {
+      return;
     }
 
-    public async init() {
-        this.documentObserver.init();
-        this.tonConnectObserver.init();
-        this.webAppObserver.init();
-        this.webViewObserver.init();
-        this.tappsObserver.init()
+    this.appModule.collectEvent(event_name, data);
 
-        try {
-            this.eventsThreshold = await (
-                await fetch(
-                    (this.appModule.env === 'STG' ? STAGING_BACKEND_URL : BACKEND_URL) + 'events/threshold',
-                    {
-                        signal: AbortSignal.timeout(2000),
-                    }
-                )
-            ).json();
-        } catch (e) {
-            this.eventsThreshold = {
-                'app-hide': 3,
-            };
-        }
-
+    if (this.eventsThreshold[event_name]) {
+      this.eventsThreshold[event_name]--;
     }
+  }
 
-    public recordEvent(event_name: string, data?: Record<string, any>) {
-        this.appModule.recordEvent(event_name, data).catch(e => console.error(e));
-    }
-
-    public collectEvent(event_name: string, data?: Record<string, any>) {
-        if (this.eventsThreshold[event_name] === 0) {
-            return;
-        }
-
-        this.appModule.collectEvent(event_name, data);
-
-        if (this.eventsThreshold[event_name]) {
-            this.eventsThreshold[event_name]--;
-        }
-    }
-
-    public collectTappsEvent(event_name: string, data?: Record<string, any>) {
-        this.appModule.collectTappsEvent(event_name, data);
-    }
+  public collectTappsEvent(event_name: string, data?: Record<string, any>) {
+    this.appModule.collectTappsEvent(event_name, data);
+  }
 }
