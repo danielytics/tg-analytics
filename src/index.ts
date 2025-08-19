b9b8575cb995883750206bdeb0156a726bf69d4a
dan <danielytics@users.noreply.github.com>
2025-08-19 22:01:37 +0100

    Fixed export

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import { App } from "./app";
-import { InvoicePayload } from "./declarations/invoice-payload.interface";
-import { validateInvoicePayload } from "./validators/invoice-payload.validator";
-import type { Events } from "./constants";
-export type { Events };
+import { App } from './app';
+import { InvoicePayload } from './declarations/invoice-payload.interface';
+import { validateInvoicePayload } from './validators/invoice-payload.validator';
+import { Events } from './constants';
+export { Events };
 
 let __registerInvoice: (invoicePayload: InvoicePayload) => void;
 
 async function init({
-  token,
-  appName,
-  wasmUrl,
-  events,
-  env = "PROD",
+    token,
+    appName,
+    wasmUrl,
+    events,
+    env = 'PROD',
 }: {
-  token: string;
-  appName: string;
-  wasmUrl: string;
-  events: Events[];
-  env?: "STG" | "PROD";
+    token: string;
+    appName: string;
+    wasmUrl: string;
+    events: Events[];
+    env?: 'STG' | 'PROD';
 }) {
-  const app = new App(token, appName, env, wasmUrl, events);
+    const app = new App(token, appName, env, wasmUrl, events);
 
-  __registerInvoice = (invoicePayload: InvoicePayload) => {
-    validateInvoicePayload(invoicePayload);
-    app.registerInvoice(invoicePayload);
-  };
+    __registerInvoice = (invoicePayload: InvoicePayload) => {
+        validateInvoicePayload(invoicePayload);
+        app.registerInvoice(invoicePayload);
+    };
 
-  await app.init();
+    await app.init();
 }
 
 export default {
-  init,
-  registerInvoice: (invoicePayload: InvoicePayload) => __registerInvoice(invoicePayload),
+    init,
+    registerInvoice: (invoicePayload: InvoicePayload) => __registerInvoice(invoicePayload),
 };
